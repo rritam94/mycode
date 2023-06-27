e0e056b68031a4690c361e9c66db253b185f4a69
rritam94 <rritam94@gmail.com>
2023-06-26 20:53:03 -0400

    Added Raw Stock Predicting Script

--- leetcode/test.java
+++ leetcode/test.java
@@ -2,20 +2,42 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args){
-        System.out.println('g');
         // System.out.println(maximumNumberOfStringPairs(new String [] {"cd","ac","dc","ca","zz"}));
+        System.out.println();
     }
 
-    // public static int maximumNumberOfStringPairs(String[] words) {
-    //     int count = 0;
-    //     for(int i = 0; i < words.length; i++){
-    //         for(int x = i + 1; x < words.length; x++){
-    //             if(new StringBuilder(words[x]).reverse().toString().equals(new StringBuilder(words[i]).reverse().toString())){
-    //                 count++;
-    //             }
-    //         }
-    //     }
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+       int left = 0;
+       int right = k - 1;
+       int max = Integer.MIN_VALUE;
+       List<Integer> l = new ArrayList<>();
+       boolean check = false;
+
+        while(right < nums.length){
+            if(!check){
+                for(int i = left; i <= right; i++){
+                    max = Math.max(max, nums[i]);
+                }
+                check = true;
+                l.add(max);
+            }
+
+            else {
+                System.out.println(left - 1);
+                if(max != nums[left - 1]){
+                    max = Math.max(nums[right], max);
+                }
+                else{
+                    for(int i = left; i <= right; i++){
+                        max = Math.max(max, nums[i]);
+                    }
+                }
+                l.add(max);
+            }
+
+            left++; right++;
+        }
         
-    //     return count;
-    // }
+        return l.stream().mapToInt(Integer::intValue).toArray();
+    }
 }
