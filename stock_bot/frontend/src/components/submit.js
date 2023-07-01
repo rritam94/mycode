7dbb10445c384b823155e8b181557cde49c30c4d
rritam94 <rritam94@gmail.com>
2023-07-01 19:26:26 -0400

    fixed json bugs and made prog towards ui

--- stock_bot/frontend/src/components/submit.js
+++ stock_bot/frontend/src/components/submit.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import { CircularProgress } from '@mui/material';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SendIcon from '@mui/icons-material/Send';
+
+const button_theme = createTheme({
+  palette: {primary: {main: '#3CD070'}, text: {primary: '#ffffff'}}
+});
 
 function App() {
   const [prediction, setPrediction] = useState({});
@@ -26,24 +33,25 @@ function App() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Stock Symbol:
-          <input type="text" name="stockSymbol" required />
-        </label>
-        <button type="submit">Predict</button>
-      </form>
+    <div className = "ret"> 
+      <div className = "results">
+        {(
+          <div> 
+            <h2>Prediction Results</h2>
+            <p>Next Day Open: {prediction.next_day_open}</p>
+            <p>Next Day Close: {prediction.next_day_close}</p>
+          </div>
+        )}
+      </div>
 
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <div> 
-          <h2>Prediction Results</h2>
-          <p>Next Day Open: {prediction.next_day_open}</p>
-          <p>Next Day Close: {prediction.next_day_close}</p>
-        </div>
-      )}
+      <div className = "form">
+        <form onSubmit={handleSubmit}>
+          <input className = "input" type="text" name="stockSymbol" required /> 
+          <ThemeProvider theme = {button_theme}>
+            <Button className = "button" variant="contained" type = "submit"> <SendIcon /> </Button>
+          </ThemeProvider>
+        </form>        
+      </div>
     </div>
   );
 }
