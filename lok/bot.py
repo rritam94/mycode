1a8258167b233c5f5d9bcfb8d61d8baaaa8f8f41
Aidan <datbluemushroom@gmail.com>
2023-08-10 23:34:57 -0400

    ✨ slash command

--- lok/bot.py
+++ lok/bot.py
@@ -6,4 +6,10 @@ bot = commands.Bot()
 async def ping(ctx):
     await ctx.send(f"Pong! Latency is {round(bot.latency * 1000)}ms")
 
-bot.run('MTEzOTM4NTQyMjM5MzM4NDk2MA.GpK3IJ.0gp9HdxACSgL9K7UwehyGm_Mmc1wD8-VDUPBmA')
+# Pycord Docs : https://guide.pycord.dev/interactions/application-commands/slash-commands
+# reference : https://stackoverflow.com/questions/71165431/how-do-i-make-a-working-slash-command-in-discord-py
+@bot.slash_command(name="first_slash", guild_ids=[...]) # Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def first_slash(ctx): 
+    await ctx.respond("You executed the slash command!")
+
+bot.run("dont leak this :D")
